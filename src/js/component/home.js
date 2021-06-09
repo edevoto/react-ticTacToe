424e0c72ddbff502f1897ba0df336342dc2d5410
enzo devoto <enzodevotob@gmail.com>
2021-06-09 16:11:09 -0400

    Modal con formulario  creado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,43 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+export function Formulario() {
+	const [opened, setOpened] = useState(true);
+
+	return opened ? (
+		<div className="pickContainer">
+			<form className="form-control bg-danger pickPlayer mt-4">
+				<label className="label"></label>
+				<div className="row form-group">
+					<input
+						type="text"
+						className="form-control col-6"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+						placeholder="first Player"
+					/>
+					<input
+						type="text"
+						className="form-control col-6"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+						placeholder="first Player"
+					/>
+				</div>
+				<div className="row">
+					<button
+						type="button"
+						className="btn btn-lg btn-light"
+						onClick={() => setOpened(false)}>
+						PLAY
+					</button>
+				</div>
+			</form>
 		</div>
+	) : (
+		<button type="button" onClick={() => setOpened(true)}>
+			Open
+		</button>
 	);
 }
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { Formulario } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Formulario />, document.querySelector("#app"));
